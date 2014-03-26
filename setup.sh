3848c22e164fd7351a307e390ac07ac55979fa9b
Mark Derricutt <mark.derricutt@smxemail.com>
2014-03-27 08:48:58 +1300

    Updated to 3.21.500
    https://github.com/Frege/frege/releases/tag/3.21.500

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
- mvn com.savage7.maven.plugins:maven-external-dependency-plugin:resolve-external
- mvn com.savage7.maven.plugins:maven-external-dependency-plugin:resolve-external
- 
+mvn com.savage7.maven.plugins:maven-external-dependency-plugin:resolve-external
